3b086fd262675704b48b0c9841585e8567319a50
Stefaan Ponnet <stefaan@ponnet.com>
2015-10-08 21:53:10 +0200

    cam fix ?

--- app/scripts/camera.js
+++ app/scripts/camera.js
@@ -5,48 +5,59 @@ var camera = (function(p_vid_id, p_inter, p_scale) {
 		return;
 	}
 
-	var vid_id   = "camsource";
+	var vid_id = "camsource";
 	var interval = p_inter != undefined ? p_inter : 1000;
-	var scale    = p_scale != undefined ? p_scale : 0.5;
+	var scale = p_scale != undefined ? p_scale : 0.5;
 
-    var video    = document.getElementById("camsource");
-    console.log('video', video);
-    var int_id   = null;
+	var video = document.getElementById("camsource");
+	console.log('video', video);
+	var int_id = null;
 
 	function start() {
-		int_id = setInterval(function(video, scale) { capture() }, interval);
+		int_id = setInterval(function(video, scale) {
+			capture()
+		}, interval);
 	}
 
 	function stop() {
-		console.log("Clearing interval with id "+int_id);
+		console.log("Clearing interval with id " + int_id);
 		clearInterval(int_id);
 	}
 
 	function capture() {
-		//console.time('capture');
-	    var w = video.videoWidth * scale;
-	    var h = video.videoHeight * scale;
-	    var qr_can = document.getElementById('qr-canvas').getContext('2d');
-	        qr_can.drawImage(video, 0, 0, w, h);
-	    try { 
-	    	var test = qrcode.decode();
-	    	console.log(test);
-	    	// fire event 'qrcoderead'
-			var event = new CustomEvent("qrcoderead", { "detail": test });
-			// Dispatch/Trigger/Fire the event
-			document.dispatchEvent(event);  
+		if (!video) {
+			video = document.getElementById("camsource");
 		}
-	    catch(err) { $("#qr-value").text(err); 
-		console.log('error? ',err);}
+		if (!video) {
+			console.log('cannot find camsource element');
+		}
+		//console.time('capture');
+		var w = video.videoWidth * scale;
+		var h = video.videoHeight * scale;
+		var qr_can = document.getElementById('qr-canvas').getContext('2d');
+		qr_can.drawImage(video, 0, 0, w, h);
+		try {
+			var test = qrcode.decode();
+			console.log(test);
+			// fire event 'qrcoderead'
+			var event = new CustomEvent("qrcoderead", {
+				"detail": test
+			});
+			// Dispatch/Trigger/Fire the event
+			document.dispatchEvent(event);
+		} catch (err) {
+			$("#qr-value").text(err);
+			console.log('error? ', err);
+		}
 		//console.timeEnd('capture');
-	} 
+	}
 
 	return {
-		interval:interval,
-		scale:scale,
-		start:start,
-		stop:stop,
-		capture:capture
+		interval: interval,
+		scale: scale,
+		start: start,
+		stop: stop,
+		capture: capture
 	}
 
 })
